e8f55ae34ed849f4a836fb5bbf51d2a9fbe18907
vicmarchesi <victor.marchesi91@gmail.com>
2024-11-26 18:13:47 +0000

    game js

---
+++ js/game.js
@@ -0,0 +1,64 @@
+class Game {
+    constructor() {
+      this.startScreen = document.getElementById("game-intro");
+      this.gameScreen = document.getElementById("game-screen");
+      this.gameEndScreen = document.getElementById("game-end");
+      this.player = new Player(this.gameScreen,
+        200,
+        500,
+        100,
+        150,
+        "./images/car.png");
+      this.height = 600;
+      this.width = 500;
+      this.obstacles = [];
+      this.score = 0;
+      this.lives = 3;
+      this.gameIsOver = false;
+      this.gameIntervalId;
+      this.gameLoopFrequency = Math.round(1000/60); // 60fps
+    }
+  
+    start() {
+      // Set the height and width of the game screen
+      this.gameScreen.style.height = `${this.height}px`;
+      this.gameScreen.style.width = `${this.width}px`;
+  
+      // Hide the start screen
+      this.startScreen.style.display = "none";
+      document.getElementById('game-container').style.display = 'flex';
+      
+      // Show the game screen
+      this.gameScreen.style.display = "block";
+  
+      // Runs the gameLoop on a fequency of 60 times per second. Also stores the ID of the interval.
+      this.gameIntervalId = setInterval(() => {
+        this.gameLoop()
+      }, this.gameLoopFrequency)
+    }
+  
+    gameLoop() {
+      console.log("in the game loop");
+      
+      this.update();
+  
+      // If "gameIsOver" is set to "true" clear the interval to stop the loop
+      if (this.gameIsOver) {
+        clearInterval(this.gameIntervalId)
+      }
+    }
+  
+    update() { // keep track of the different parts of the game updates
+      console.log("in the update");
+      this.player.move();
+
+      for (let i = 0; i < this.obstacles.length; i++) {
+        const obstacle = this.obstacles[i];
+        obstacle.move();
+      }
+
+      if (Math.random() > 0.98 && this.obstacles.length < 5) {
+        this.obstacles.push(new Obstacle(this.gameScreen));
+      }
+    }
+  }
